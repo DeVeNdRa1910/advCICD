5beed10435fd5a6fbcd1d939706b2dfb734a322b
Devendra <jaybantv@gmail.com>
2025-06-17 22:03:59 +0530

    Change the accessible of port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import authRouter from './routes/auth.js';
 import postsRouter from './routes/posts.js';
 import { connectToRedis } from './services/redis.js';
 const app = express();
-const port = PORT || 5000;
+export const port = PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('Yay!! Backend of wanderlust app is now accessible');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
 
