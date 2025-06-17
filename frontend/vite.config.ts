457e7b6b0945c95fa10e202a8ea82a37d7801f11
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-06-17 18:38:33 +0530

    Update vite.config.ts
    update vite.config.ts file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '0.0.0.0', // Accept connections from all interfaces (important for Docker/K8s)
+    port: 5173,      // Ensure it listens on this port
+    strictPort: true // Fails fast if port 5173 is already in use
+  }
 });
